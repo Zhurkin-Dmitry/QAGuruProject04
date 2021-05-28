460ba99891bee84c6fe3049bf720e47636f402c4
Zhurkin-Dmitriy <zhurkindo@gmail.com>
2021-05-28 22:57:44 +0300

    second point of home work

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -1,4 +1,24 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GitHubTest {
 
+    @Test
+    void gitHubSearch() {
+        open("https://github.com/selenide/selenide");
+        $("[data-content=\"Wiki\"]").click();
+        $(byText("Show 1 more pages…")).click();
+        $$("#wiki-pages-box li a").find(text("SoftAssertions")).click();
+        $$("#wiki-body ol li").shouldHave(itemWithText("Using JUnit5 extend test class:"));
+
+        sleep(4000);
+    }
 
 }
